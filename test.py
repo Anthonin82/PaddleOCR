4bd78fc34c3c761236caf3d4a2fd4221420b32a5
Anthonin82 <anthonin.i82@gmail.com>
2024-01-10 03:56:38 +0100

    e

--- test.py
+++ test.py
@@ -6,10 +6,5 @@ from paddleocr import PaddleOCR,draw_ocr
 # Paddleocr supports Chinese, English, French, German, Korean and Japanese.
 # You can set the parameter `lang` as `ch`, `en`, `fr`, `german`, `korean`, `japan`
 # to switch the language model in order.
-ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
-img_path = '._PMC1626454_002_00.png'
-result = ocr.ocr(img_path, cls=True)
-for idx in range(len(result)):
-    res = result[idx]
-    for line in res:
-        print(line)
+# Predict table image
+tools/infer_table.py -c configs/table/SLANet.yml -o Global.pretrained_model={path/to/weights}/best_accuracy  Global.infer_img=ppstructure/docs/table/table.jpg
